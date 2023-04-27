720ff22966648255c935c690691f7b3087249f92
Troy Dieter <troydieter@gmail.com>
2023-04-26 21:58:35 -0400

    dropped unnecessary vars

--- aws-terraform-hybrid-dns/variables.tf
+++ aws-terraform-hybrid-dns/variables.tf
@@ -22,23 +22,3 @@ variable "region_secondary" {
   default     = "us-east-2"
   description = "The secondary region to deploy to"
 }
-
-variable "accepter_vpc_id" {
-  description = "VPC id that you want to peer with it"
-  type        = string
-}
-
-variable "accepter_route_table_id" {
-  description = "Route table id of the accepter that you want to peer with it"
-  type        = string
-}
-
-variable "onpremdnsa_priv_ip" {
-  description = "Private IP Address of micros4l-onpremdnsa"
-  type        = string
-}
-
-variable "onpremdnsb_priv_ip" {
-  description = "Private IP Address of micros4l-onpremdnsb"
-  type        = string
-}
